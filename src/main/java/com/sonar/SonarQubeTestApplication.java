17188b75f7d00fef2c33a2c4ab676ea62ec2d758
rasel <md.rasel@machette.tech>
2021-05-03 19:55:25 +0600

    first commit

---
+++ src/main/java/com/sonar/SonarQubeTestApplication.java
@@ -0,0 +1,13 @@
+package com.sonar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonarQubeTestApplication {
+
+	public static void main(String[] args) { //NOSONAR
+		SpringApplication.run(SonarQubeTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sonar/SonarQubeTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sonar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SonarQubeTestApplicationTests {
+
+	@Test //NOSONAR
+	void contextLoads() {
+	}
+
+}
